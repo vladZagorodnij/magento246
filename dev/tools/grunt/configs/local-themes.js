a22c8ee35bc3ee74f91afef7bab64c0226388da2
Dockerizer <email@example.com>
2023-09-13 18:53:50 +0300

    enabled grunt

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Define Themes
+ *
+ * area: area, one of (frontend|adminhtml|doc),
+ * name: theme name in format Vendor/theme-name,
+ * locale: locale,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+module.exports = {
+    myCustomTheme: {
+        area: 'frontend',
+        name: 'VladZahorodnii/myCustomTheme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    }
+};
