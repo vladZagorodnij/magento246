2ea4c65b9903076fdb4ac6102a027490fda4305d
Dockerizer <email@example.com>
2023-09-18 23:11:10 +0300

    added url to categories data, added to template

--- app/code/VladZahorodnii/HomeCategory/Model/AllCategory.php
+++ app/code/VladZahorodnii/HomeCategory/Model/AllCategory.php
@@ -4,37 +4,56 @@ declare(strict_types=1);
 namespace VladZahorodnii\HomeCategory\Model;
 
 use Exception;
-use Magento\Catalog\Api\CategoryListInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Catalog\Api\Data\CategorySearchResultsInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class AllCategory
 {
     /**
-     * @var SearchCriteriaBuilder
+     * @var \Magento\Framework\Api\SearchCriteriaBuilder
      */
-    private SearchCriteriaBuilder $searchCriteriaBuilder;
+    private \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder;
 
     /**
-     * @var CategoryListInterface
+     * @var \Magento\Catalog\Api\CategoryListInterface
      */
-    private CategoryListInterface $categoryList;
+    private \Magento\Catalog\Api\CategoryListInterface $categoryList;
 
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private \Magento\Store\Model\StoreManagerInterface $storeManager;
+
+    /**
+     * @var \Magento\Catalog\Model\CategoryRepository
+     */
+    private \Magento\Catalog\Model\CategoryRepository $categoryRepository;
+
+    /**
+     * @param \Magento\Catalog\Api\CategoryListInterface $categoryList
+     * @param \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Catalog\Model\CategoryRepository $categoryRepository
+     */
     public function __construct(
-        CategoryListInterface $categoryList,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        \Magento\Catalog\Api\CategoryListInterface $categoryList,
+        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\CategoryRepository $categoryRepository,
     ) {
         $this->categoryList = $categoryList;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->storeManager = $storeManager;
+        $this->categoryRepository = $categoryRepository;
     }
 
     /**
-     * @return CategorySearchResultsInterface
+     * @return array
      * @throws Exception
      */
-    public function getAllSystemCategory():CategorySearchResultsInterface
+    public function getAllSystemCategory(): array
     {
-        $categoryList = [];
+        $categoriesList = [];
+
         try {
             $searchCriteria = $this->searchCriteriaBuilder->create();
             $categoryList = $this->categoryList->getList($searchCriteria);
@@ -42,6 +61,33 @@ class AllCategory
             throw new Exception($exception->getMessage());
         }
 
-        return $categoryList;
+        if ($categoryList->getTotalCount()) {
+            foreach ($categoryList->getItems() as $category){
+                $categoriesList[] = $category->getData();
+            }
+        }
+
+        return $this->getCategoryUrl($categoriesList);
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     */
+    public function getCategoryUrl($categoriesList): array
+    {
+        $categoriesListWithUrl = [];
+
+        foreach ($categoriesList as $category) {
+            $categoryId = $category['entity_id'];
+
+            $categoryUrl = $this->categoryRepository->get($categoryId, $this->storeManager->getStore()->getId())->getUrl();
+
+            if (str_ends_with($categoryUrl, 'html')) {
+                $category['url'] = $categoryUrl;
+                $categoriesListWithUrl[] = $category;
+            }
+        }
+
+        return $categoriesListWithUrl;
     }
 }
