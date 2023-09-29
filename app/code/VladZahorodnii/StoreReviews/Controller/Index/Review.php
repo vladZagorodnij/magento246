3e731da44d0978950dc99b4dd10b2ab6592b7cc4
Dockerizer <email@example.com>
2023-09-30 02:34:16 +0300

    added primitive ajax request

---
+++ app/code/VladZahorodnii/StoreReviews/Controller/Index/Review.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace VladZahorodnii\StoreReviews\Controller\Index;
+
+class Review implements \Magento\Framework\App\Action\HttpPostActionInterface
+{
+    public function execute()
+    {
+        $foo = '';
+
+        return $this;
+
+    }
+}
--- app/code/VladZahorodnii/StoreReviews/view/frontend/web/js/store-review-submit.js
+++ app/code/VladZahorodnii/StoreReviews/view/frontend/web/js/store-review-submit.js
@@ -1,33 +1,101 @@
 define([
     'jquery',
+    'mage/translate',
+    'Magento_Customer/js/customer-data',
+    'mage/storage',
+    'mage/url',
     'validation',
     'jquery-ui-modules/widget'
-], function ($) {
+], function ($, $t, customerData, storage, urlBuilder) {
     'use strict';
 
-    $.widget('dv.storeReviewSubmit', {
+    $.widget('vladZahorodnii.storeReviewSubmit', {
+        options: {
+            action: urlBuilder.build(`store_review/index/review`)
+        },
 
         /**
          * Initialize
          */
         _init: function () {
+            this.addEventListeners();
+        },
+
+        /**
+         * addEventListeners
+         */
+        addEventListeners() {
             this._on(this.element, {
                 submit: this.onSubmit.bind(this)
             })
         },
 
+        /**
+         * onSubmit
+         * @param event
+         * @returns {vladZahorodnii.storeReviewSubmit}
+         */
         onSubmit(event) {
             event.preventDefault();
 
             if (!this.element.validation('isValid')) {
-                console.log('form is not valid!');
-
-                return this;
+                throw new Error($t(`Form is not valid!`))
             }
 
-            console.log('form is valid? send Ajax!')
+            this.sendAjax();
+        },
+
+        sendAjax() {
+            // return storage.post()
+            let formData = new FormData($(this.element).get(0));
+
+            return $.ajax({
+                url: this.options.action,
+                data: formData,
+                processData: false,
+                contentType: false,
+                dataType: 'json',
+                type: 'post',
+                beforeSend: this.beforeAjax.bind(this),
+                error: this.onAjaxError.bind(this),
+                success: this.onAjaxSuccess.bind(this),
+                afterSend: this.afterAjax.bind(this)
+            })
+        },
+
+        beforeAjax() {
+            $('body').trigger('processStart');
+        },
+
+        onAjaxError(response) {
+            this.showMessage(response.responseJSON.message ?? $t('Oops, something went wrong!'))
+        },
+
+        onAjaxSuccess(response) {
+            this.showMessage($t('Your review was sent! Thank you!'), 'success');
+        },
+
+        afterAjax() {
+            $('body').trigger('processStop');
+        },
+
+        showMessage(text='', type='error') {
+            if (!text) return this;
+
+            const section = customerData.get('message')() ?? {};
+            const messages = section.messages ?? [];
+
+            customerData.set('messages', {
+                messages: [
+                    ...messages,
+                    {
+                        text,
+                        type
+                    }
+                ]
+            })
         }
     });
 
-    return $.dv.storeReviewSubmit;
+    return $.vladZahorodnii.storeReviewSubmit;
 });
